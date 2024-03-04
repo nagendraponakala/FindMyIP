9e27e8c5d95ee0ec227ee7e9fabeab8cb21b3c5f
Nagendra Ponakala <nagendra.ponakala@gmail.com>
2024-03-05 01:01:30 +0530

    refactor detail screen

--- feature/findmyip/src/main/kotlin/com/photon/feature/findmyip/FindMyIpScreen.kt
+++ feature/findmyip/src/main/kotlin/com/photon/feature/findmyip/FindMyIpScreen.kt
@@ -10,17 +10,23 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -42,9 +48,10 @@ fun FindMyIpRoute(
 internal fun FindMyIpScreen(
     uiState: FindMyIpUiState
 ) {
-    Surface(
-        color = MaterialTheme.colorScheme.surface
-    ) {
+    Column(
+        modifier = Modifier
+            .background(color = MaterialTheme.colorScheme.surface)
+    ){
         TopAppBar()
         when(uiState){
             is FindMyIpUiState.Error -> {
@@ -71,7 +78,7 @@ internal fun FindMyIpScreen(
             }
             is FindMyIpUiState.Success -> {
                 Box(
-                    contentAlignment = Alignment.Center,
+                    contentAlignment = Alignment.TopStart,
                     modifier = Modifier.fillMaxSize()
                 ) {
                     FindMyIpDetailsScreen(uiState.data)
@@ -83,26 +90,37 @@ internal fun FindMyIpScreen(
 
 @Composable
 fun FindMyIpDetailsScreen(data: IpData) {
-    val headers = listOf("IP", "City", "State", "Country", "ZipCode")
-    val values = listOf(data.ip, data.city, data.region, data.countryName, data.postal)
-    Card(
+    val dataAsList: List<Pair<String, String>> = listOf(
+        "IP" to data.ip,
+        "City" to data.city,
+        "State" to data.region,
+        "Country" to data.countryName,
+        "ZipCode" to data.postal
+    )
+    OutlinedCard(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp)
+            .padding(16.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+            contentColor = MaterialTheme.colorScheme.onSurface
+        )
     ) {
-        headers.forEachIndexed { index, label ->
-            Column {
+        dataAsList.forEach { it ->
+            Column{
                 Row(
+                    modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     Text(
-                        text = label,
+                        text = it.first,
                         fontSize = 16.sp,
-                        fontWeight = FontWeight.Bold,
+                        fontWeight = FontWeight.Bold
+                        ,
                         modifier = Modifier.padding(16.dp)
                     )
                     Text(
-                        text = "${values[index]}",
+                        text = it.second,
                         fontSize = 16.sp,
                         modifier = Modifier.padding(16.dp)
                     )
@@ -138,4 +156,40 @@ fun TopAppBar(
             modifier = Modifier.padding(start = 8.dp, end = 8.dp)
         )
     }
+}
+
+
+
+@Preview
+@Composable
+fun FindMyIpScreenPreview() {
+    FindMyIpScreen(
+        uiState = FindMyIpUiState.Success(
+            data = IpData(
+                ip = "124.123.178.88",
+                city = "Hyderabad",
+                region = "Telangana",
+                countryName = "India",
+                postal = "500002",
+                latitude = 17.3724,
+                longitude = 78.4378
+            )
+        )
+    )
+}
+
+@Preview
+@Composable
+fun FindMyIpDetailsScreenPreview() {
+    FindMyIpDetailsScreen(
+        data = IpData(
+            ip = "124.123.178.88",
+            city = "Hyderabad",
+            region = "Telangana",
+            countryName = "India",
+            postal = "500002",
+            latitude = 17.3724,
+            longitude = 78.4378
+        )
+    )
 }
